cc506c3538137f1530901740b6afe8c8f5bac06b
Cold7 <contrerasriquelme.sebastian@gmail.com>
2016-06-01 12:43:52 -0300

    Add files via upload

--- necessaryLibs.py
+++ necessaryLibs.py
@@ -6,8 +6,22 @@ for fichero in listdir("."):
 		File=open(fichero)
 		for line in File:
 			if "import" in line:
-				if line not in libs:
-					libs.append(line[:-1])
+				if "#" in line:
+					Line=line[:-1]
+					l=Line[0:((Line.index("#")))]
+					flag=0
+					for char in l:
+						if char!=" ":
+							flag=1
+							break
+					if len(l)>0:
+						flag=1
+					
+					if l not in libs and flag==1:
+						libs.append(l)
+				else:
+					if line[0:-1] not in libs:
+						libs.append(line[:-1])
 		File.close()
 for lib in libs:
 	print lib
